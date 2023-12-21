115c741ff26eb7723e744bdacdb9c8c895d486bc
Domenik1ink <39694133+Domenik1ink@users.noreply.github.com>
2023-12-21 20:25:50 +0100

    Http handling

--- src/app/api-handler.service.ts
+++ src/app/api-handler.service.ts
@@ -1,20 +1,33 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiHandlerService {
+    domain:string = "localhost";
+    port:number = 4201;
+    endpoint:string = '';
 
-    constructor(private httpClient:HttpClient) {}
+
+    constructor(private httpClient:HttpClient, @Inject(DOCUMENT) private document:any) {
+        this.domain = this.document.location.hostname;
+        if(this.domain === 'localhost') {
+            this.domain = 'http://' + this.domain;
+        } else{
+            this.domain = 'https://'  + this.domain;
+        }
+        this.endpoint = this.domain + ':' + this.port;
+    }
 
     getCalendar(year:number):Observable<MonthDto[]> {
-        return this.httpClient.request<MonthDto[]>("GET", "http://localhost:8080/getCalendar?year=" + year);
+        return this.httpClient.request<MonthDto[]>("GET", `${this.endpoint}/getCalendar?year=${year}`);
     }
 
     getCalendarEvents(year:number):Observable<EventDto> {
-        return this.httpClient.request<EventDto>("GET", "http://localhost:8080/getYearEvents?year=" + year)
+        return this.httpClient.request<EventDto>("GET", `${this.endpoint}/getYearEvents?year=${year}`)
     }
 }
 
